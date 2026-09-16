c3e2c980eec37df96e2ad4df27e444e269e26f5e
FireBrand Student <firebrand@student.local>
2026-09-16 15:35:10 +0100

    meal interface

--- recipe-finder/src/app/app.ts
+++ recipe-finder/src/app/app.ts
@@ -1,6 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+export interface Meal{
+  idMeal: number,
+  strMeal?: string,
+  strCategory?: string,
+  strArea?: string,
+  strInstructions?: string,
+  strMealThumb?: string,
+  strYoutube?: string,
+  strImageSource?: string,
+}
+
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
