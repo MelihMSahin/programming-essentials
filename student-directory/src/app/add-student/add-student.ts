d272fbe9137dcfd080370df02c8ae7206be19c63
FireBrand Student <firebrand@student.local>
2026-09-17 14:52:42 +0100

    validation to add student

--- student-directory/src/app/add-student/add-student.ts
+++ student-directory/src/app/add-student/add-student.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 export interface NewStudent {
   name: string;
@@ -7,24 +7,42 @@ export interface NewStudent {
 }
 
 @Component({
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   selector: 'app-add-student',
   styleUrl: './add-student.css',
   templateUrl: './add-student.html',
   standalone: true,
 })
 export class AddStudent {
-  studentName = '';
-  studentScore: number | null = null;
-
   @Output() studentAdded = new EventEmitter<NewStudent>();
 
   onSubmit() {
-    if (this.studentName.trim() && this.studentScore !== null) {
-      this.studentAdded.emit({
-        name: this.studentName.trim(),
-        score: this.studentScore,
-      });
+    if (this.addStudentForm.invalid) {
+      this.addStudentForm.markAllAsTouched();
+      return;
     }
+
+    const { name, score } = this.addStudentForm.getRawValue();
+    this.studentAdded.emit({
+      name: name.trim(),
+      score: Number(score),
+    });
+    this.addStudentForm.reset();
   }
+
+  addStudentForm = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
+    score: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.pattern('^[0-9]+$'),
+        Validators.min(0),
+        Validators.max(100),
+      ],
+    }),
+  });
 }
