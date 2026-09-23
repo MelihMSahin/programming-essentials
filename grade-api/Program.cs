c90f0a8c4a8d7f099ed8441cd3b8e306b7970ad3
FireBrand Student <firebrand@student.local>
2026-09-23 09:43:39 +0100

    sql_lite base set-up

--- grade-api/Program.cs
+++ grade-api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=students.db"));
 
 var app = builder.Build();
 
